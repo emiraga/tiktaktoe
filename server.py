5d61ab48a43774f0d3afe497d24de266a4b9b956
Emir Habul <emiraga@gmail.com>
2021-09-05 20:34:31 +0200

    Move status logic to server; Restart button; Add bunch of TODOs

--- server.py
+++ server.py
@@ -30,14 +30,25 @@ condition = threading.Condition()
 
 @app.route("/play-move")
 def play_move():
-	global squares
-	global x_is_next
+	global squares, x_is_next
 	move = int(request.args['move'])
 	player = request.args['player']
 	player_is_x = player == 'X'
-	if player_is_x == x_is_next and squares[move] is None:
+	if (player_is_x == x_is_next and
+			squares[move] is None and
+			not computeStatus()['winning_player']):
 		squares[move] = player
 		x_is_next = not x_is_next
+		with condition:
+			condition.notify_all()
+	return ''
+
+@app.route("/reset-game")
+def reset_game():
+	global squares, x_is_next
+	if computeStatus()['is_restartable']:
+		squares = [None] * 9
+		x_is_next = True
 		with condition:
 			condition.notify_all()
 	return ''
@@ -52,8 +63,70 @@ def stream():
 				'player_code': player_code,
 				'squares': squares,
 				'x_is_next': x_is_next,
+				'status': computeStatus(),
 			}))
 			with condition:
 				condition.wait()
 
 	return Response(eventStream(), mimetype="text/event-stream")
+
+
+
+def calculateIsBoardFilled():
+	for sq in squares:
+		if not sq:
+			return False
+	return True
+
+
+def calculateWinner():
+	lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	]
+	for [a,b,c] in lines:	
+		if squares[a] and squares[a] == squares[b] and squares[a] == squares[c]:
+			return squares[a]
+	return None
+
+
+def computeStatus():
+	player = calculateWinner()
+	winning_player = None
+	if player:
+		winning_player = player
+		message = 'Winner ' + player
+		is_restartable = True
+	elif calculateIsBoardFilled():
+		message = 'Nobody wins :('
+		is_restartable = True
+	else:
+		message = 'Next player: ' + ('X' if x_is_next else 'O')
+		is_restartable = False
+	return {
+		'winning_player': winning_player,
+		'message': message,
+		'is_restartable': is_restartable,
+	}
+
+'''
+TODO:
+ - change first player turn (first time X, second time O is first)
+ - keep score for players
+ - multiple games
+ - password per player per game
+ - connection reset handling
+ - choose local versus networked game
+ - AI opponent, minimax search
+ - add locking for all operations. a = threading.Lock()
+
+TODO cleanup:
+ - reduce number of global variable
+ - use class for better abstraction
+'''
--- tiktakgame.js
+++ tiktakgame.js
@@ -39,34 +39,34 @@ function Game(props) {
   var [playerCode, setPlayerCode] = React.useState(false);
   var [squares, setSquares] = React.useState(Array(9).fill(null));
   var [xIsNext, setXIsNext] = React.useState(true);
+  var [status, setStatus] = React.useState({});
 
-  React.useEffect(() => {
-    var eventSource = new EventSource("/stream");
-    eventSource.onmessage = (e) => {
-      const data = JSON.parse(e.data);
-      setPlayerCode(data.player_code);
-      setSquares(data.squares);
-      setXIsNext(data.x_is_next);
-    };
-    return () => eventSource.close();
-  }, []);
+  React.useEffect(
+    () => {
+      var eventSource = new EventSource("/stream");
+      eventSource.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        setPlayerCode(data.player_code);
+        setSquares(data.squares);
+        setXIsNext(data.x_is_next);
+        setStatus(data.status);
+      };
+      return () => eventSource.close();
+    },
+    []
+  );
 
   function handleClick(i){
-    if (calculateWinner(squares) || squares[i]){
+    if (status.winning_player || squares[i]){
       return;
     }
     fetch('/play-move?player=' + playerCode + '&move=' + i)
+      .then(r => null, e => console.log(e));
   }
 
-  const winner = calculateWinner(squares);
-
-  let status;
-  if (winner) {
-    status = 'Winner ' + winner;
-  } else if (calculateIsDraw(squares)) {
-    status = 'Nobody wins :(';
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  function handleReset() {
+    fetch('/reset-game')
+      .then(r => null, e => console.log(e));
   }
 
   if (!playerCode) {
@@ -75,6 +75,11 @@ function Game(props) {
         <p>Waiting on the server...</p>
       </div>
     )
+  }
+
+  let reset_button = <React.Fragment />;
+  if (status.is_restartable) {
+    reset_button = <input type="button" value="Reset" onClick={() => handleReset()} ></input>;
   }
 
   return (
@@ -87,39 +92,11 @@ function Game(props) {
       </div>
       <div className="game-info">
         <div>You are: {playerCode}</div>
-        <div>{status}</div>
+        <div>{status.message}</div>
+        {reset_button}
       </div>
     </div>
   );
-}
-
-function calculateIsDraw(squares) {
-  for (let i=0; i<squares.length; i++){
-    if (!squares[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i=0; i<lines.length; i++){
-    const [a,b,c] = lines[i];
-    if (squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 ReactDOM.render(
